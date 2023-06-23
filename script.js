af9fc8d746f43291d3e41ef80a16f0f88381108a
skay <kherdinsameera@gmail.com>
2023-06-23 12:18:00 +0300

    second_change

--- script.js
+++ script.js
@@ -1,54 +1,94 @@
-const inputBox =document.getElementById("input-box");
-const listContainer=document.getElementById("list-container");
-const validation=document.querySelector(".validation");
 
+const inputBox =document.getElementsByClassName('input-box')[0];
+
+const addTaskBtn = document.getElementsByClassName('btn')[0];
+
+
+const validation=document.querySelector(".validation");
 
 inputBox.addEventListener('keyup', (event) => {
     if (event.keyCode === 13) {
+        
         addTask();
+        
     }
 });
 
-function addTask(){
-    if(inputBox.value ===''){
+function addTask()
+{
+ if(inputBox.value ===''){
         // alert("You must write something");
         validation.classList.add('show')
+        setTimeout(function(){document.querySelector(".validation").style.display="none";},3000);
     }
-    else{
+    if(inputBox.value.trim()!=0){
+           let localItems = JSON.parse( localStorage.getItem('localItem'))
+        if(localItems === null){
+             taskList = []
+    
+        }else{
+            taskList = localItems;
+        }
+        taskList.push(inputBox.value)
+        localStorage.setItem('localItem', JSON.stringify(taskList)); 
+    }
+    
+        showItem()
+}
+addTaskBtn.addEventListener('click', function (){
+  
+    addTask();
+    })
+
+    function showItem(){
+        let localItems = JSON.parse( localStorage.getItem('localItem'))
+        if(localItems === null){
+             taskList = []
+    
+        }else{
+            taskList = localItems;
+        }
+    
+    
+    let html = '';
+    let itemShow = document.querySelector('.todoLists');
+    taskList.forEach((data, index )=> {
         
-        let li =document.createElement("li");
-        li.innerHTML =inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML ="\u00d7"; 
-        li.appendChild(span);
+    
+        html += `
+        <div class="todoList">
+    
+        <p class="pText">${data}</p>
+    
+    
+        </div>
+        
+        <button class="delete" onClick="deleteItem(${index})">x</button>
+        
+        `
+    })
+    itemShow.innerHTML = html;
     }
-    inputBox.value="";
-    saveData();
-}
 
-
-
-
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
+    showItem()
+    function deleteItem(index){
+        let localItems = JSON.parse( localStorage.getItem('localItem'))
+        taskList.splice(index, 1)
+        localStorage.setItem('localItem', JSON.stringify(taskList));
+        showItem()
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
+    
+    function clearTask(){
+        
+    localStorage.clear()
+    showItem()
     }
-},false);
 
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
-}
-function showTask()
-{
-    listContainer.innerHTML= localStorage.getItem("data");
-}
-showTask();
+
+
+
+
+
 
 
