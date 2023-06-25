4e9dae3b623542c041e3b906066b8b4ef897b41f
skay <kherdinsameera@gmail.com>
2023-06-25 17:25:02 +0300

    third

--- script.js
+++ script.js
@@ -1,94 +1,102 @@
+  // Retrieve tasks from local storage or create an empty array
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-const inputBox =document.getElementsByClassName('input-box')[0];
+  // Function to save tasks to local storage
+  function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 
-const addTaskBtn = document.getElementsByClassName('btn')[0];
+  // Function to render tasks
+  function renderTasks() {
+    const taskList = document.getElementById('task-list');
+    const filterRadio = document.querySelector('input[name="filter"]:checked');
+    const filter = filterRadio ? filterRadio.value : 'all';
 
+    taskList.innerHTML = '';
 
-const validation=document.querySelector(".validation");
+    tasks.forEach((task, index) => {
+      if (filter === 'all' || (filter === 'done' && task.done) || (filter === 'undone' && !task.done)) {
+        const taskItem = document.createElement('li');
+        taskItem.className = 'task-item' + (task.done ? ' done' : '');
 
-inputBox.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-        
-        addTask();
-        
-    }
-});
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = task.done;
+        checkbox.addEventListener('change', () => {
+          task.done = checkbox.checked;
+          saveTasks();
+          renderTasks();
+        });
 
-function addTask()
-{
- if(inputBox.value ===''){
-        // alert("You must write something");
-        validation.classList.add('show')
-        setTimeout(function(){document.querySelector(".validation").style.display="none";},3000);
-    }
-    if(inputBox.value.trim()!=0){
-           let localItems = JSON.parse( localStorage.getItem('localItem'))
-        if(localItems === null){
-             taskList = []
-    
-        }else{
-            taskList = localItems;
-        }
-        taskList.push(inputBox.value)
-        localStorage.setItem('localItem', JSON.stringify(taskList)); 
-    }
-    
-        showItem()
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.value = task.text;
+        input.addEventListener('change', () => {
+          task.text = input.value;
+          saveTasks();
+        });
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+          tasks.splice(index, 1);
+          saveTasks();
+          renderTasks();
+        });
+
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(input);
+        taskItem.appendChild(deleteButton);
+        taskList.appendChild(taskItem);
+      }
+    });
+  }
+
+ // Function to display an error message
+function displayErrorMessage(message) {
+const errorMessage = document.getElementById('error-message');
+errorMessage.textContent = message;
+
+if (message !== '') {
+  setTimeout(() => {
+    errorMessage.textContent = '';
+  }, 3000); // Delay in milliseconds (3 seconds in this case)
 }
-addTaskBtn.addEventListener('click', function (){
-  
-    addTask();
-    })
+}
 
-    function showItem(){
-        let localItems = JSON.parse( localStorage.getItem('localItem'))
-        if(localItems === null){
-             taskList = []
-    
-        }else{
-            taskList = localItems;
-        }
-    
-    
-    let html = '';
-    let itemShow = document.querySelector('.todoLists');
-    taskList.forEach((data, index )=> {
-        
-    
-        html += `
-        <div class="todoList">
-    
-        <p class="pText">${data}</p>
-    
-    
-        </div>
-        
-        <button class="delete" onClick="deleteItem(${index})">x</button>
-        
-        `
-    })
-    itemShow.innerHTML = html;
+  // Function to add a new task
+  function addTask() {
+    const input = document.getElementById('new-task-input');
+    const text = input.value.trim();
+
+    if (text !== '') {
+      tasks.push({ text, done: false });
+      saveTasks();
+      input.value = '';
+      renderTasks();
+      displayErrorMessage(''); // Clear any previous error message
+    } else {
+      displayErrorMessage('You must enter a task'); // Display the error message
     }
+  }
 
-    showItem()
-    function deleteItem(index){
-        let localItems = JSON.parse( localStorage.getItem('localItem'))
-        taskList.splice(index, 1)
-        localStorage.setItem('localItem', JSON.stringify(taskList));
-        showItem()
+  // Event listener for add task button
+  const addTaskBtn = document.getElementById('add-task-btn');
+  addTaskBtn.addEventListener('click', addTask);
+
+  // Event listener for enter key press in the input field
+  const newTaskInput = document.getElementById('new-task-input');
+  newTaskInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      addTask();
     }
-    
-    function clearTask(){
-        
-    localStorage.clear()
-    showItem()
-    }
+  });
 
+  // Event listener for filter change
+  const filterRadios = document.querySelectorAll('input[name="filter"]');
+  filterRadios.forEach((radio) => {
+    radio.addEventListener('change', renderTasks);
+  });
 
-
-
-
-
-
-
-
+  // Initial rendering of tasks
+  renderTasks();
